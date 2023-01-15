4f9853e11437daa82fca6933782632653ee6ddd3
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-01-16 04:37:23 +0500

    Update ChatInput.tsx

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -1,12 +1,21 @@
 'use client'
 
-import React from 'react'
+import React, { FormEvent } from 'react'
 
 const ChatInput = () => {
+  const [input, setInput] = React.useState("")
+  const addMessage = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!input) {
+      return
+    }
+    const messageToSend = input
+    setInput("")
+  }
   return (
-    <form className='fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-200'>
-        <input type="text" placeholder='Enter message here...' className='flex-1 rounded border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed' />
-        <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed'> Send </button>
+    <form onSubmit={addMessage} className='fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-200'>
+        <input type="text" value={input} onChange={e => setInput(e.target.value)} placeholder='Enter message here...' className='flex-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed' />
+        <button type='submit' disabled={!input} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed'> Send </button>
     </form>
   )
 }
