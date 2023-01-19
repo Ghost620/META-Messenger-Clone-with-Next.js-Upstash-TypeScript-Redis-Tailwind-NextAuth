b993f089ac61cda397aae7c6d8a0d78f094a5bb9
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-01-20 01:08:19 +0500

    chat styling

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -49,9 +49,9 @@ const ChatInput = () => {
   }
 
   return (
-    <form onSubmit={addMessage} className='fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-200'>
+    <form onSubmit={addMessage} className='fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t bg-white border-gray-200'>
         <input type="text" value={input} onChange={e => setInput(e.target.value)} placeholder='Enter message here...' className='flex-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed' />
-        <button type='submit' disabled={!input} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed'> Send </button>
+        <button type='submit' disabled={!input} className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed'> Send </button>
     </form>
   )
 }
--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -8,7 +8,7 @@ import MessageComponent from "./MessageComponent";
 const MessageList = () => {
   const { data: messages, error, mutate } = useSWR<Message[]>("/api/getMessages", fetcher)
   return (
-    <div>
+    <div className="space-y-5 px-5 pt-8 pb-32 max-w-2xl xl:max-w-4xl mx-auto">
       {messages?.map(msg => (
         <MessageComponent key={msg.id} message={msg} />
       ))}
