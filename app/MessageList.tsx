4ea01ca00ffc79e2a826d56482a99c1857643e8d
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-01-21 16:49:42 +0500

    loading status added

--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -7,7 +7,11 @@ import { Message } from "../typings";
 import fetcher from '../utils/fetchMessages'
 import MessageComponent from "./MessageComponent";
 
-const MessageList = () => {
+type Props = {
+  initialMessages: Message[]
+}
+
+const MessageList = ({initialMessages}: Props) => {
   const { data: messages, error, mutate } = useSWR<Message[]>("/api/getMessages", fetcher)
 
   useEffect(() => {
@@ -36,7 +40,7 @@ const MessageList = () => {
 
   return (
     <div className="space-y-5 px-5 pt-8 pb-32 max-w-2xl xl:max-w-4xl mx-auto">
-      {messages?.map(msg => (
+      {(messages || initialMessages).map(msg => (
         <MessageComponent key={msg.id} message={msg} />
       ))}
     </div>
---
+++ app/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex flex-col items-center align-middle h-screen'>
+        <div>
+            Loading Messages ...
+        </div>
+    </div>
+  )
+}
+
+export default loading
